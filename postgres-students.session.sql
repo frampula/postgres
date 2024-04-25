7b816f539db0d8c26f7c81ed013170dcdad2fd14
framupla <kidalo1337@gmail.com>
2024-04-25 19:24:17 +0300

    Mini YouTube table

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,35 +1,19 @@
-CREATE TABLE chats(
+CREATE TABLE contents(
     id serial PRIMARY KEY,
     name varchar(256) NOT NULL CHECK(name != ''),
-    owner_id int REFERENCES users(id),
+    description text,
+    author_id serial REFERENCES users(id),
     created_at timestamp DEFAULT current_timestamp
-);
+)
 
-INSERT INTO chats(name, owner_id) VALUES -- створення чату
-('superchat', 2);
-
-CREATE TABLE chats_to_users(
-    chat_id int REFERENCES chats(id),
+CREATE TABLE reactions(
+    content_id int REFERENCES contents(id),
     user_id int REFERENCES users(id),
-    join_at timestamp DEFAULT current_timestamp,
-    PRIMARY KEY (chat_id, user_id)
-);
+    is_liked boolean
+)
 
-INSERT INTO chats_to_users(chat_id, user_id) VALUES -- додавання до чату учасників
-(2, 2);
+INSERT INTO contents(name, author_id) VALUES -- Add content
+('Funny dogs', 3)
 
-CREATE TABLE messages(
-    id serial PRIMARY KEY,
-    body text NOT NULL CHECK(body != ''),
-    created_at timestamp DEFAULT current_timestamp,
-    is_read boolean NOT NULL DEFAULT false,
-    -- author_id int REFERENCES chats_to_users(user_id),
-    -- chat_id int REFERENCES chats_to_users(chat_id)
-    author_id int,
-    chat_id int,
-    FOREIGN KEY (author_id, chat_id) REFERENCES chats_to_users(user_id, chat_id)
-);
-
-INSERT INTO messages(body, author_id, chat_id) VALUES -- додавання повідомлень до чату
-('go for coffee?', 4, 2),
-('go', 2, 2);
+INSERT INTO reactions VALUES -- Add reaction
+(1, 1, true)
