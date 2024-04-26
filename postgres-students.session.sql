247e50f863601f784d8797abfdea9364989c84ab
framupla <kidalo1337@gmail.com>
2024-04-26 18:26:59 +0300

    Generated column - welcome

--- postgres-students.session.sql
+++ postgres-students.session.sql
@@ -1,19 +1,10 @@
-CREATE TABLE contents(
+CREATE TABLE products_2(
     id serial PRIMARY KEY,
-    name varchar(256) NOT NULL CHECK(name != ''),
-    description text,
-    author_id serial REFERENCES users(id),
-    created_at timestamp DEFAULT current_timestamp
+    brand varchar(256) NOT NULL CHECK (brand != ''),
+    model varchar(256) NOT NULL CHECK (model != ''),
+    price numeric(8, 2) NOT NULL,
+    is_luxury boolean GENERATED ALWAYS AS (price > 800) STORED
 )
 
-CREATE TABLE reactions(
-    content_id int REFERENCES contents(id),
-    user_id int REFERENCES users(id),
-    is_liked boolean
-)
-
-INSERT INTO contents(name, author_id) VALUES -- Add content
-('Funny dogs', 3)
-
-INSERT INTO reactions VALUES -- Add reaction
-(1, 1, true)
+ALTER TABLE products
+ADD COLUMN is_luxury boolean GENERATED ALWAYS AS (price > 800) STORED
