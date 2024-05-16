91fc087d084d4af91f8327f2c060033005bf5660
framupla <kidalo1337@gmail.com>
2024-05-16 17:49:20 +0300

    idk again

--- normalize.sql
+++ normalize.sql
@@ -1,45 +1,34 @@
-CREATE TABLE students (
+CREATE TABLE products(
     id serial PRIMARY KEY,
-    name varchar(30)
-);
-
-CREATE TABLE teachers (
-    id serial PRIMARY KEY,
-    name varchar(30),
-    subject varchar(50) REFERENCES subjects(name)
-);
-
-CREATE TABLE subjects (
-    name varchar(50) PRIMARY KEY
+    name varchar(300), CHECK (name != '') NOT NULL
 )
 
-CREATE TABLE students_to_teachers(
-    teacher_id int REFERENCES teachers(id),
-    student_id int REFERENCES students(id),
-    PRIMARY KEY (teacher_id, student_id)
-);
+CREATE TABLE manufacturers(
+    id serial PRIMARY KEY,
+    name varchar(400) CHECK (name != '') NOT NULL,
+    adress text CHECK (adress != '') NOT NULL,
+    tel_number varchar(15) CHECK (tel_number != '') NOT NULL
+)
 
-INSERT INTO students_to_teachers VALUES
-(5, 1),
-(5, 2),
-(6, 1)
+CREATE TABLE orders (
+    id serial PRIMARY KEY,
+    product_id int REFERENCES products(id),
+    quantity_plan int CHECK (quantity_plan > 0) NOT NULL,
+    product_price decimal(10, 2),
+    contract_number varchar(30) NOT NULL,
+    contract_adress text CHECK (contract_adress != '') NOT NULL,
+    contract_date date NOT NULL,
+    manufacturer_id int REFERENCES manufacturers(id)
+)
 
-INSERT INTO students (name) VALUES 
-('Ivanon'),
-('Petrov'),
-('Sidorov')
+CREATE TABLE shipments (
+    id serial PRIMARY KEY,
+    order_id int REFERENCES orders(id),
+    shipment_date timestamp NOT NULL
+)
 
-INSERT INTO teachers (name, subject) VALUES 
-('Smirnov', 'Системы ИИ'),
-('Petrenko', 'Облачное программирование')
-
-INSERT INTO subjects VALUES
-('Системы ИИ'),
-('Облачное программирование')
-
-SELECT students.id, students.name, teachers.name, teachers.subject FROM 
-students JOIN students_to_teachers
-ON students.id = students_to_teachers.student_id
-JOIN teachers
-ON students_to_teachers.teacher_id = teachers.id
-
+CREATE TABLE products_to_shipments(
+    product_id int REFERENCES products(id),
+    shipment_id int REFERENCES shipments(id),
+    product_quantity int NOT NULL
+)
